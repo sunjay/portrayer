99da2bade37caab083804e06520adf3eb7b61e84
Sunjay Varma <varma.sunjay@gmail.com>
2019-07-11 15:55:11 -0400

    Attempting to map normals on a sphere

--- src/primitive/sphere.rs
+++ src/primitive/sphere.rs
@@ -56,21 +56,35 @@ impl RayHit for Sphere {
         let t = equation.solve().find(|sol| t_range.contains(sol))?;
 
         let hit_point = ray.at(t);
+        let tex_coord = Uv {
+            // Using spherical coordinates.
+            // Formula from Fundamentals of Computer Graphics, 4th ed. Chapter 11.2.1
+            // The addition/subtraction and the division maps the angles to the 0.0 to 1.0 range
+            // Signs of x, y, z adjusted to account for axis convention
+            u: (PI + (-hit_point.z).atan2(hit_point.x)) / (2.0 * PI),
+            v: hit_point.y.acos() / PI,
+        };
+        // Normal of sphere is the hit point on the sphere minus the center (0, 0, 0)
+        // Note that we do not divide by the radius because the radius is 1.0
+        let normal = hit_point;
+
+        // Can create a tangent space using the normal based on our knowledge that the sphere is
+        // axis-aligned. That means that we can find a horizontal tangent (aligned with the x-z
+        // plane) by taking the cross product of the normal with a vector with a higher y value.
+        let horizontal_tangent = normal.cross(normal + Vec3 {x: 0.0, y: 0.1, z: 0.0}).normalized();
+        let second_tangent = normal.cross(horizontal_tangent);
+        let normal_map_transform = Mat3::from_col_arrays([
+            horizontal_tangent.into_array(),
+            normal.into_array(),
+            second_tangent.into_array(),
+        ]);
+
         Some(RayIntersection {
             ray_parameter: t,
             hit_point,
-            // Normal of sphere is the hit point on the sphere - the center (0, 0, 0)
-            // Note that we do divide by the radius because the radius is 1.0
-            normal: hit_point,
-            tex_coord: Some(Uv {
-                // Using spherical coordinates.
-                // Formula from Fundamentals of Computer Graphics, 4th ed. Chapter 11.2.1
-                // The addition/subtraction and the division maps the angles to the 0.0 to 1.0 range
-                // Signs of x, y, z adjusted to account for axis convention
-                u: (PI + (-hit_point.z).atan2(hit_point.x)) / (2.0 * PI),
-                v: hit_point.y.acos() / PI,
-            }),
-            normal_map_transform: Some(Mat3::identity()), //TODO
+            normal,
+            tex_coord: Some(tex_coord),
+            normal_map_transform: Some(normal_map_transform), //TODO
         })
     }
 }
