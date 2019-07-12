b40626f39aa961d1775ec33c85efb38784cc9122
Sunjay Varma <varma.sunjay@gmail.com>
2019-07-12 12:01:20 -0400

    Setting up scene for normal mapping comparision shots

--- examples/normal-mapping.rs
+++ examples/normal-mapping.rs
@@ -1,11 +1,13 @@
 //! Demonstrates the normal mapping feature on all supported primitives
+//!
+//! Shows all the same objects from two different light angles to show how the shadows change
 
 use std::error::Error;
 use std::sync::Arc;
 
 use portrayer::{
     scene::{HierScene, SceneNode, Geometry},
-    primitive::{Sphere, Cube, FinitePlane},
+    primitive::{Primitive, Cube, FinitePlane, Sphere},
     material::Material,
     texture::{Texture, ImageTexture},
     light::Light,
@@ -17,130 +19,138 @@ use portrayer::{
 use image::RgbImage;
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let mat_mirror = Arc::new(Material {
-        diffuse: Rgb {r: 0.0, g: 0.0, b: 0.0},
-        specular: Rgb {r: 0.6, g: 0.6, b: 0.6},
-        shininess: 1000.0,
-        reflectivity: 1.0,
-        ..Material::default()
-    });
-    let mat_wood = Arc::new(Material {
-        diffuse: Rgb {r: 0.545, g: 0.353, b: 0.169},
-        specular: Rgb {r: 0.5, g: 0.7, b: 0.5},
-        shininess: 25.0,
-        ..Material::default()
-    });
+    let tex_map = Arc::new(Texture::from(ImageTexture::open("assets/Metal_Tiles_001_basecolor.jpg")?));
+    let norm_map = Arc::new(Texture::from(ImageTexture::open("assets/Metal_Tiles_001_normal.jpg")?));
 
-    let earth = Arc::new(Texture::from(ImageTexture::open("assets/earth.jpg")?));
     let mat_tex = Arc::new(Material {
-        diffuse: Rgb {r: 0.506, g: 0.78, b: 0.518},
-        specular: Rgb {r: 0.5, g: 0.5, b: 0.5},
+        diffuse: Rgb {r: 0.37168, g: 0.236767, b: 0.692066},
+        specular: Rgb {r: 0.3, g: 0.3, b: 0.3},
         shininess: 25.0,
-        texture: Some(earth.clone()),
+        texture: Some(tex_map.clone()),
+        ..Material::default()
+    });
+    let mat_tex_norm = Arc::new(Material {
+        diffuse: Rgb {r: 0.37168, g: 0.236767, b: 0.692066},
+        specular: Rgb {r: 0.3, g: 0.3, b: 0.3},
+        shininess: 25.0,
+        texture: Some(tex_map.clone()),
+        normals: Some(norm_map),
         ..Material::default()
     });
 
-    let earth_normals = Arc::new(Texture::from(ImageTexture::open("assets/earth_normals.jpg")?));
-    let mat_tex_norms = Arc::new(Material {
-        diffuse: Rgb {r: 0.506, g: 0.78, b: 0.518},
-        specular: Rgb {r: 0.5, g: 0.5, b: 0.5},
-        shininess: 25.0,
-        texture: Some(earth),
-        normals: Some(earth_normals),
-        ..Material::default()
-    });
+    let tex_map_cube = Arc::new(Texture::from(ImageTexture::open("assets/Metal_Tiles_001_basecolor_cubemap.png")?));
+    let norm_map_cube = Arc::new(Texture::from(ImageTexture::open("assets/Metal_Tiles_001_normal_cubemap.png")?));
 
-    let earth_cubemap = Arc::new(Texture::from(ImageTexture::open("assets/earth_cube.png")?));
     let mat_tex_cube = Arc::new(Material {
-        diffuse: Rgb {r: 0.506, g: 0.78, b: 0.518},
-        specular: Rgb {r: 0.5, g: 0.5, b: 0.5},
+        diffuse: Rgb {r: 0.37168, g: 0.236767, b: 0.692066},
+        specular: Rgb {r: 0.3, g: 0.3, b: 0.3},
         shininess: 25.0,
-        texture: Some(earth_cubemap.clone()),
+        texture: Some(tex_map_cube.clone()),
+        ..Material::default()
+    });
+    let mat_tex_norm_cube = Arc::new(Material {
+        diffuse: Rgb {r: 0.37168, g: 0.236767, b: 0.692066},
+        specular: Rgb {r: 0.3, g: 0.3, b: 0.3},
+        shininess: 25.0,
+        texture: Some(tex_map_cube.clone()),
+        normals: Some(norm_map_cube),
         ..Material::default()
     });
 
-    let earth_cubemap_normals = Arc::new(Texture::from(ImageTexture::open("assets/earth_cube_normals.png")?));
-    let mat_tex_cube_norms = Arc::new(Material {
-        diffuse: Rgb {r: 0.506, g: 0.78, b: 0.518},
-        specular: Rgb {r: 0.5, g: 0.5, b: 0.5},
+    let mat_wall_floor = Arc::new(Material {
+        diffuse: Rgb {r: 0.424858, g: 0.531206, b: 0.8},
+        specular: Rgb {r: 0.3, g: 0.3, b: 0.3},
         shininess: 25.0,
-        texture: Some(earth_cubemap),
-        normals: Some(earth_cubemap_normals),
         ..Material::default()
     });
 
-    let mirror = Arc::new(SceneNode::from(Geometry::new(Cube, mat_wood.clone()))
-        .scaled((9.0, 0.5, 6.0))
-        .rotated_x(Radians::from_degrees(10.0))
-        .with_child(
-            SceneNode::from(Geometry::new(Cube, mat_mirror))
-                .scaled((8.1/9.0, 0.05/0.5, 5.4/6.0))
-                .translated((0.0, 0.27/0.5, 0.0))
-        ));
+    let mut nodes = Vec::new();
+
+    // Walls + Floor
+    nodes.push(
+        SceneNode::from(Geometry::new(FinitePlane, mat_wall_floor.clone()))
+            .scaled(30.0)
+            .into()
+    );
+    nodes.push(
+        SceneNode::from(Geometry::new(Cube, mat_wall_floor.clone()))
+            .scaled((0.1, 20.0, 20.0))
+            .translated((0.0, 8.0, 0.0))
+            .into()
+    );
+    nodes.push(
+        SceneNode::from(Geometry::new(Cube, mat_wall_floor.clone()))
+            .scaled((30.0, 30.0, 0.4))
+            .translated((0.0, 8.0, -10.0))
+            .into()
+    );
+
+    // Each primitive in a column and its scale, rotation, and height
+    let from_degrees = Radians::from_degrees;
+    let no_scale = Vec3::one();
+    let zero_rot = vek::Vec3 {x: from_degrees(0.0), y: from_degrees(0.0), z: from_degrees(0.0)};
+    let prim_scale_rotation_height: &[(Primitive, _, _, _)] = &[
+        (FinitePlane.into(), Vec3 {x: 2.2, y: 1.0, z: 2.0}, vek::Vec3 {x: from_degrees(90.0), ..zero_rot}, 10.9),
+        (Sphere.into(), no_scale, zero_rot, 7.9),
+        (Cube.into(), Vec3::from(2.0), zero_rot, 4.9),
+        (Cube.into(), Vec3::from(2.0), vek::Vec3 {y: from_degrees(180.0), ..zero_rot}, 1.9),
+    ];
+
+    // The x coordinate of the center of each column and the material to use for that column
+    let col_x_mat_mat_cube = &[
+        (-4.0, mat_tex.clone(), mat_tex_cube.clone()),
+        (-1.5, mat_tex_norm.clone(), mat_tex_norm_cube.clone()),
+        (1.5, mat_tex_norm.clone(), mat_tex_norm_cube.clone()),
+        (4.0, mat_tex.clone(), mat_tex_cube.clone()),
+    ];
+
+    for &(x, ref mat, ref mat_cube) in col_x_mat_mat_cube {
+        for &(ref prim, scale, rot, height) in prim_scale_rotation_height {
+            let mat = match prim {
+                Primitive::Cube(_) => mat_cube.clone(),
+                _ => mat.clone(),
+            };
+
+            nodes.push(
+                SceneNode::from(Geometry::new(prim.clone(), mat))
+                    .scaled(scale)
+                    .rotated_xzy(rot)
+                    .translated((x, height, 0.0))
+                    .into()
+            );
+        }
+    }
 
     let scene = HierScene {
-        root: SceneNode::from(vec![
-            mirror,
-
-            SceneNode::from(Geometry::new(FinitePlane, mat_tex.clone()))
-                .scaled((4.0, 1.0, 2.0))
-                .rotated_x(Radians::from_degrees(90.0))
-                .translated((-2.1, 2.0, -2.0))
-                .into(),
-            SceneNode::from(Geometry::new(FinitePlane, mat_tex_norms.clone()))
-                .scaled((4.0, 1.0, 2.0))
-                .rotated_x(Radians::from_degrees(90.0))
-                .translated((2.1, 2.0, -2.0))
-                .into(),
-
-            SceneNode::from(Geometry::new(Cube, mat_tex_cube.clone()))
-                .scaled(1.4)
-                .translated((-3.1, 2.0, 0.0))
-                .into(),
-            SceneNode::from(Geometry::new(Cube, mat_tex_cube_norms.clone()))
-                .scaled(1.4)
-                .translated((-1.1, 2.0, 0.0))
-                .into(),
-
-            SceneNode::from(Geometry::new(Sphere, mat_tex.clone()))
-                .scaled(0.8)
-                .translated((1.1, 2.0, 0.0))
-                .into(),
-            SceneNode::from(Geometry::new(Sphere, mat_tex_norms.clone()))
-                .scaled(0.8)
-                .translated((3.1, 2.0, 0.0))
-                .into(),
-        ]).into(),
+        root: SceneNode::from(nodes).into(),
 
         lights: vec![
+            // Left Point Light
             Light {
-                position: Vec3 {x: -6.0, y: 5.0, z: 4.0},
-                color: Rgb {r: 0.5, g: 0.5, b: 0.5},
-                ..Light::default()
-            },
-            Light {
-                position: Vec3 {x: 6.0, y: 5.0, z: 4.0},
-                color: Rgb {r: 0.5, g: 0.5, b: 0.5},
+                position: Vec3 {x: -10.0, y: 9.0, z: 7.0},
+                color: Rgb {r: 0.9, g: 0.9, b: 0.9},
                 ..Light::default()
             },
 
+            // Right Point Light
             Light {
-                position: Vec3 {x: 0.0, y: 1.0, z: -4.0},
-                color: Rgb {r: 0.5, g: 0.5, b: 0.5},
+                position: Vec3 {x: 10.0, y: 9.0, z: 7.0},
+                color: Rgb {r: 0.9, g: 0.9, b: 0.9},
                 ..Light::default()
             },
         ],
-        ambient: Rgb {r: 0.3, g: 0.3, b: 0.3},
+
+        ambient: Rgb {r: 0.1, g: 0.1, b: 0.1},
     };
 
     let cam = CameraSettings {
-        eye: (0.0, 10.15667, 11.579666).into(),
-        center: (0.0, -5.913023, -7.571445).into(),
+        eye: (0.0, 6.04746, 17.688135).into(),
+        center: (0.0, 6.047459, -23.311874).into(),
         up: Vec3::up(),
-        fovy: Radians::from_degrees(25.0),
+        fovy: Radians::from_degrees(40.0),
     };
 
-    let mut image = RgbImage::new(910, 512);
+    let mut image = RgbImage::new(910, 1024);
 
     image.render::<RenderProgress, _>(&scene, cam,
         |uv: Uv| Rgb {r: 0.2, g: 0.4, b: 0.6} * (1.0 - uv.v) + Rgb::blue() * uv.v);
