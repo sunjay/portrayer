9db05f9602c867d7e89cb51ef240d29eff3d5bb4
Sunjay Varma <varma.sunjay@gmail.com>
2019-07-13 23:34:46 -0400

    Initial pass at implementing transmission / refraction

--- src/material.rs
+++ src/material.rs
@@ -8,8 +8,44 @@ use crate::scene::Scene;
 use crate::ray::{Ray, RayCast};
 use crate::texture::{Texture, NormalMap, TextureSource};
 
-// Controls the maximum ray recursion depth
+/// Controls the maximum ray recursion depth
 const MAX_RECURSION_DEPTH: u32 = 10;
+
+/// Index of refraction of air
+pub const AIR_REFRACTION_INDEX: f64 = 1.00;
+/// Index of refraction of water
+pub const WATER_REFRACTION_INDEX: f64 = 1.33;
+/// Index of refraction of window glass
+pub const WINDOW_GLASS_REFRACTION_INDEX: f64 = 1.51;
+/// Index of refraction of optical glass
+pub const OPTICAL_GLASS_REFRACTION_INDEX: f64 = 1.92;
+/// Index of refraction of diamond
+pub const DIAMOND_REFRACTION_INDEX: f64 = 2.42;
+
+/// Returns the direction of the transmitted / refracted ray (normalized) or None if there is
+/// total internal reflection
+fn refracted_direction(ray_dir: Vec3, normal: Vec3, refraction_index: f64) -> Option<Vec3> {
+    // This formula is from section 13.1 in Fundamentals of Computer Graphics, 4th Ed.
+
+    // The greek letter "eta" is used for the refraction index
+    let eta = refraction_index;
+
+    // We are assuming that outside the surface is air
+    let eta_outside = AIR_REFRACTION_INDEX;
+
+    let ray_dot_norm = ray_dir.dot(normal);
+    let under_sqrt = 1.0 - eta_outside*eta_outside * (1.0 - ray_dot_norm*ray_dot_norm)/(eta*eta);
+    if under_sqrt < 0.0 {
+        // Total internal reflection
+        return None;
+    }
+
+    // The direction of refracted / transmitted ray
+    // Two variables for the two halfs of the equation
+    let refracted_dir_1 = eta_outside * (ray_dir - normal*ray_dot_norm) / eta;
+    let refracted_dir_2 = normal * under_sqrt.sqrt();
+    Some(refracted_dir_1 - refracted_dir_2)
+}
 
 #[derive(Debug, Clone, Default, PartialEq)]
 pub struct Material {
@@ -27,9 +63,16 @@ pub struct Material {
     /// * 10,000 - nearly mirror-like
     pub shininess: f64,
     /// The reflectivity of this material, used to blend in the color from the reflected ray
+    ///
+    /// If this material also has a non-zero refraction index, this will be used to blend in the
+    /// total color from both the reflected ray and the refracted ray.
     pub reflectivity: f64,
     /// The side length of the glossy reflection rectangle
     pub glossy_side_length: f64,
+    /// The index of refraction inside the surface with this material
+    ///
+    /// It is assumed that the outside of the surface has index of refraction = 1.0 (air)
+    pub refraction_index: f64,
     /// The texture to sample the diffuse color from
     pub texture: Option<Arc<Texture>>,
     /// The texture to sample the shading normal from
@@ -161,6 +204,8 @@ impl Material {
         if self.reflectivity > 0.0 {
             // r = v - 2N(v dot N) where v = ray direction, N = normal
             let mut reflect_dir = ray_dir - normal * 2.0 * ray_dir.dot(normal);
+
+            // Perturb the reflection ray if we are using glossy reflection
             if self.glossy_side_length > 0.0 {
                 // Create a basis u, v from the ideal reflection ray
                 // This is a technique for creating a basis from a single vector:
@@ -184,7 +229,79 @@ impl Material {
             // Add reflection via recursive ray tracing
             let reflected_ray = Ray::new(hit_point, reflect_dir);
             let reflected_color = reflected_ray.color(scene, background, recursion_depth + 1);
-            color += reflected_color * self.reflectivity;
+
+            // This code is translated from pseudo code in Section 13.1 of
+            // Fundamentals of Computer Graphics, 4th Ed.
+            if self.refraction_index > 0.0 {
+                // Dielectric material
+
+                // The reflectivity of a dielectric varies with the incident angle according to the
+                // Fresnel equations. We use the Schlick approximation which uses the cosine of the
+                // incident angle.
+                let refract_dir_cos_incident = if ray_dir.dot(normal) < 0.0 {
+                    // Ray is going into the surface
+
+                    // Refracted / transmitted ray
+                    let refract_dir = refracted_direction(ray_dir, normal, self.refraction_index)
+                        .expect("bug: should not have total internal reflection when casting inside surface");
+                    // Incident angle here is the angle between the ray and the normal. Ray is
+                    // reversed because it is currently pointing towards the surface and we want
+                    // the other angle.
+                    let cos_incident = (-ray_dir).dot(normal);
+
+                    Some((refract_dir, cos_incident))
+                } else if let Some(refract_dir) = refracted_direction(ray_dir, -normal, 1.0/self.refraction_index) {
+                    // Ray is heading outside the surface
+
+                    // Since the ray is coming from inside the surface, the light (which is on the
+                    // outside) is actually incident with the refracted ray. Note that we don't
+                    // need to flip the refracted direction in this branch because it is already
+                    // pointing away from the surface (unlike the ray direction in the previous
+                    // case).
+                    let cos_incident = refract_dir.dot(normal);
+
+                    Some((refract_dir, cos_incident))
+                } else {
+                    // Total internal reflection
+
+                    // Since there is only reflection, this code is the same as the reflective-only case
+                    color += self.reflectivity * reflected_color;
+
+                    // Finish processing if this is the case
+                    None
+                };
+
+                // Only continue if there was not total internal reflection
+                if let Some((refract_dir, cos_incident)) = refract_dir_cos_incident {
+                    // Compute the reflectivity using the Schlick approximation
+
+                    // The reflectivity at normal incidence
+                    // r0 = (eta - 1)^2/(eta + 1)^2
+                    let r0 = (self.refraction_index - 1.0)*(self.refraction_index - 1.0);
+                    let r0 = r0 / ((self.refraction_index + 1.0)*(self.refraction_index + 1.0));
+                    // The reflectivity according to the approximation, distinct from the property
+                    // in the material
+                    let reflectivity = r0 + (1.0 - r0) * (1.0 - cos_incident).powi(5);
+
+                    // By conservation of energy, the energy not transmitted/refracted is reflected
+                    let transmittance = 1.0 - reflectivity;
+
+                    // Cast the transmitted ray and determine the color
+                    let refracted_ray = Ray::new(hit_point, refract_dir);
+                    let refracted_color = refracted_ray.color(scene, background, recursion_depth + 1);
+
+                    // The total color uses the result of Fresnel/Schlick to mix the reflected and
+                    // refracted/transmitted colors
+                    let total_color = reflectivity * reflected_color + transmittance * refracted_color;
+                    // Mix in the total color using the material reflectivity coefficient
+                    color += self.reflectivity * total_color;
+                }
+
+            } else {
+                // Reflective-only material
+
+                color += self.reflectivity * reflected_color;
+            }
         }
 
         color
