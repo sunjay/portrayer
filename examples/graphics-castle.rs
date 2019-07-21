fc2cb641d8cc48f6f4a91309d763edc134563689
Sunjay Varma <varma.sunjay@gmail.com>
2019-07-20 20:02:33 -0400

    Positioning the lights inside the towers

--- examples/graphics-castle.rs
+++ examples/graphics-castle.rs
@@ -41,17 +41,17 @@ fn main() -> Result<(), Box<dyn Error>> {
 
             // Dim lights inside each tower to light inside
             Light {
-                position: Vec3 {x: -42.0, y: 112.0, z: -229.0},
+                position: Vec3 {x: -30.0, y: 80.0, z: 0.0},
                 color: Rgb {r: 0.3, g: 0.3, b: 0.3},
                 ..Light::default()
             },
             Light {
-                position: Vec3 {x: 42.0, y: 112.0, z: -229.0},
+                position: Vec3 {x: 30.0, y: 80.0, z: 0.0},
                 color: Rgb {r: 0.3, g: 0.3, b: 0.3},
                 ..Light::default()
             },
             Light {
-                position: Vec3 {x: 0.0, y: 129.0, z: -260.0},
+                position: Vec3 {x: 0.0, y: 93.0, z: -23.0},
                 color: Rgb {r: 0.3, g: 0.3, b: 0.3},
                 ..Light::default()
             },
